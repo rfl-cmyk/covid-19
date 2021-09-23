b21784a9b5ae3df7aa22a986112ee2c3ec0a5e68
rfl-cmyk <rafael@sitesotimizados.com.br>
2021-09-23 09:26:55 -0300

    Mapa responsivo ok

--- src/App.js
+++ src/App.js
@@ -5,7 +5,18 @@ import Mapa from './componentes/Mapa';
 
 function App() {
 
+  const [global, setGlobal] = useState([]);
   const [casos, setCasos] = useState([]);
+
+  useEffect(() => {
+    return(
+      fetch(`https://disease.sh/v3/covid-19/all`)
+        .then(resposta => resposta.json())
+        .then(dados => {
+          setGlobal(dados)
+        })
+    )
+  }, []);
 
   useEffect(() => {
     return(
@@ -19,7 +30,7 @@ function App() {
 
   return (
     <div className="App">
-      { casos.length ? <Mapa casos={casos} /> : null } {/* utilizado devido ao useEffect preencher a variável 'casos' após a renderização do componente */}
+      { casos.length ? <Mapa global={global} casos={casos} /> : null } {/* utilizado devido ao useEffect preencher a variável 'casos' após a renderização do componente */}
     </div>
   );
 }
--- src/componentes/Mapa.js
+++ src/componentes/Mapa.js
@@ -1,43 +1,109 @@
 import { MapContainer, TileLayer, Marker, Popup } from 'react-leaflet';
 import L from 'leaflet';
-import markerIcon from 'leaflet/dist/images/marker-icon.png';
+
+import gIcon from '../images/green-icon.png';
+import oIcon from '../images/orange-icon.png';
+import rIcon from '../images/red-icon.png';
 
 import 'leaflet/dist/leaflet.css';
 import './mapa.css';
 
 function Mapa(props) {
-    const markerIconConst = L.icon({
-        iconUrl: markerIcon,
-        iconRetinaUrl: markerIcon,
-        iconAnchor: [5, 55],
-        popupAnchor: [10, -44],
-        iconSize: [25, 45],
+
+    var greenIcon = L.icon({
+        iconUrl: gIcon,    
+        iconSize:     [15, 15],
+        iconAnchor:   [0, 0],
+        popupAnchor: [8, 5]
     });
+
+    var orangeIcon = L.icon({
+        iconUrl: oIcon,    
+        iconSize:     [15, 15],
+        iconAnchor:   [0, 0],
+        popupAnchor: [8, 5]
+    });
+
+    var redIcon = L.icon({
+        iconUrl: rIcon,    
+        iconSize:     [15, 15],
+        iconAnchor:   [0, 0],
+        popupAnchor: [8, 5]
+    });
+
+    const markerColor = (a, b) => { // função responsável por determinar a cor de cada marcador
+        var dados = parseInt(b * 100 / a);
+        var icon;
+
+        if (dados >= 10) {
+            icon = redIcon;
+        } else if (dados <= 9 && dados >= 5){
+            icon = orangeIcon;
+        } else {
+            icon = greenIcon;
+        }
+
+        return icon;
+    }
   
     return (
-        <MapContainer minZoom={2} center={[ 15, 30 ]} zoom={3} scrollWheelZoom={true}>
-            <TileLayer
-              attribution='&copy; <a href="http://osm.org/copyright">OpenStreetMap</a> contributors'
-              url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
-            />
-            { props.casos.map(dados => {
-                let localizacao = [ dados.countryInfo.lat, dados.countryInfo.long ]
-                return(
-                <Marker key={dados.country} title={dados.country} riseOnHover={true} icon={markerIconConst} position={localizacao}>
-                    <Popup>
-                        <img className="flag" src={dados.countryInfo.flag} alt={dados.country} />
-                        <h3>{dados.country}</h3>
-                        <p>&rsaquo; population: <strong>{dados.population.toLocaleString('pt-BR')}</strong></p>
-                        <p>&rsaquo; total cases: <strong>{dados.cases.toLocaleString('pt-BR')}</strong></p>
-                        <p>&rsaquo; total deaths: <strong>{dados.deaths.toLocaleString('pt-BR')}</strong></p>
-                        <hr/>
-                        <p>&rsaquo; today cases: <strong>{dados.todayCases.toLocaleString('pt-BR')}</strong></p>
-                        <p>&rsaquo; today deaths: <strong>{dados.todayDeaths.toLocaleString('pt-BR')}</strong></p>
-                    </Popup>
-                </Marker>
-                )
-            })}  
-        </MapContainer>
+        <>
+
+            <div className="mapInfos">
+                <h1>Global cases of covid-19</h1>
+                <ul>
+                    <li>
+                        <h3>Population</h3>
+                        <h2 className="blue">{props.global.population.toLocaleString('pt-BR')}</h2>
+                    </li>
+                    <li>
+                        <h3>Cases</h3>
+                        <h2 className="orange">{props.global.cases.toLocaleString('pt-BR')}</h2>
+                    </li>
+                    <li>
+                        <h3>Deaths</h3>
+                        <h2 className="red">{props.global.deaths.toLocaleString('pt-BR')}</h2>
+                    </li>
+                    <li>
+                        <h3>Recovered</h3>
+                        <h2 className="green">{props.global.recovered.toLocaleString('pt-BR')}</h2>
+                    </li>
+                </ul>
+            </div>
+
+            <div className="mapSubs">
+                <ul>
+                    <li><h3>Percentage of Infections</h3></li>
+                    <li>(in relation to population)</li>
+                    <li><p>Above 10%</p></li>
+                    <li><p>Between 5 and 9%</p></li>
+                    <li><p>Under 5%</p></li>
+                </ul>
+            </div>
+
+            <MapContainer minZoom={2} maxZoom={5} center={[ 15, -7 ]} zoom={2} scrollWheelZoom={true}>
+                <TileLayer
+                attribution='&copy; <a href="http://osm.org/copyright">OpenStreetMap</a> contributors'
+                url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
+                />
+                { props.casos.map(dados => {
+                    let localizacao = [ dados.countryInfo.lat, dados.countryInfo.long ]
+                    return(
+                    <Marker key={dados.country} title={dados.country} riseOnHover={true} icon={markerColor(dados.population, dados.cases)} position={localizacao}>
+                        <Popup>
+                            <img className="flag" src={dados.countryInfo.flag} alt={dados.country} />
+                            <ul>
+                                <li><h3>{dados.country}</h3></li>
+                                <li><p>&rsaquo; population: <strong>{dados.population.toLocaleString('pt-BR')}</strong></p></li>
+                                <li><p>&rsaquo; total cases: <strong>{dados.cases.toLocaleString('pt-BR')}</strong></p></li>
+                                <li><p>&rsaquo; total deaths: <strong>{dados.deaths.toLocaleString('pt-BR')}</strong></p></li>
+                            </ul>
+                        </Popup>
+                    </Marker>
+                    )
+                })}  
+            </MapContainer>
+        </>
     );
 }
 
