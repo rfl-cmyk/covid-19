a6d6898ffebe9950aebcebd8d445a50b6bebe21f
rfl-cmyk <rafael@sitesotimizados.com.br>
2021-09-27 16:44:56 -0300

    Rodapé e load ok

--- src/App.js
+++ src/App.js
@@ -1,6 +1,8 @@
 import { useState, useEffect } from 'react';
 import './App.css';
 
+import PulseLoader from "react-spinners/PulseLoader";
+
 import Mapa from './componentes/Mapa';
 import GraficoRosca from './componentes/GraficoRosca';
 import GraficoLinha from './componentes/GraficoLinha';
@@ -10,6 +12,15 @@ function App() {
   const [global, setGlobal] = useState([]);
   const [history, setHistory] = useState([]);
   const [casos, setCasos] = useState([]);
+
+  const [loading, setLoading] = useState(false)
+
+  useEffect(() => { // loading da página
+    setLoading(true)
+    setTimeout(() => {
+      setLoading(false)
+    }, 2000)
+  }, [])
 
   useEffect(() => {
     return(
@@ -43,11 +54,26 @@ function App() {
 
   return (
     <div className="App">
-      { casos.length && Object.keys(global).length ? <Mapa global={global} casos={casos} /> : null } {/* utilizado devido ao useEffect preencher a variável 'casos' após a renderização do componente */}
+      {
+        loading ? <div className="loader"><PulseLoader color={"#1e4d6f"} loading={loading} size={15} /></div>
+        :
+        <>
+          { casos.length && Object.keys(global).length ? <Mapa global={global} casos={casos} /> : null } {/* utilizado devido ao useEffect preencher a variável 'casos' após a renderização do componente */}
 
-      { Object.keys(history).length ? <div className="graficoLinha"><GraficoLinha history={history} /></div> : null }
+          { Object.keys(history).length ? <div className="graficoLinha"><GraficoLinha history={history} /></div> : null }
 
-      { Object.keys(global).length ? <div className="graficoRosca"><GraficoRosca global={global} /></div> : null }
+          { Object.keys(global).length ? <div className="graficoRosca"><GraficoRosca global={global} /></div> : null }
+
+          <div className="rodape">
+            <p><strong>Toolbox:</strong>&nbsp;
+              <a href="https://www.npmjs.com/package/react-spinners" rel="noreferrer" target="_blank">react-spinners (loading)</a>,&nbsp;
+              <a href="https://disease.sh/" rel="noreferrer" target="_blank">disease.sh (covid api)</a>,&nbsp;
+              <a href="https://react-leaflet.js.org/" rel="noreferrer" target="_blank">Leaflet (map)</a> and&nbsp;
+              <a href="https://github.com/reactchartjs/react-chartjs-2" rel="noreferrer" target="_blank">React-chartjs2 (graphics)</a>
+            </p>
+          </div>
+        </>
+      }
     </div>
   );
 }
